18d6478dc62c5e68205affb777f85308cfce664d
lingalasowjanya0628-dev <lingalasowjanya0628@gmail.com>
2026-08-27 22:06:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+alert("Welcome to Sowjanya's Student Portfolio!");
 
+document.querySelectorAll("nav a").forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        console.log("You clicked: " + this.textContent);
+
+    });
+
+});
